1f53ca9dcc79e1b4a030d4b3b7d2ec4e7c19decb
jhlee <sunglasses99@snu.ac.kr>
2023-02-21 23:59:03 +0900

    sync with presentation slides

--- tests/unsafe.ml
+++ tests/unsafe.ml
@@ -1,10 +1,18 @@
 let f () = raise Not_found
 type t = A of (unit -> unit) | B of int
 
-let () = match (A f, 2) with
+let g = function
   | (_, 3) -> ()
-  | (A g, _) -> g ()
+  | (A h, _) -> h ()
+  | _ -> ()
 
-let () = match (A f, 2) with
-  | (A g, _) -> g ()
-  | (_, 3) -> ()
+let _ = g (A f, 2)
+
+let g (x, y) = match y with
+  | 3 -> ()
+  | _ -> (match x with
+          | A h -> h ()
+          | _ -> ())
+
+let _ = g (A f, 2)
+
